7146083f29bd4ec389e115ce45f0688771551b20
Hongyejinjin <hjinjin@ewha.ac.kr>
2026-04-09 09:10:23 +0000

    complete2

---
+++ Pass-by-reference.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+void getNumbers(int& input1, int& input2);
+void swapValues(int& varable1, int& variable2);
+void showResults(int output1, int output2);
+
+int main()
+{
+    int firstNum{}, secondNum{};
+    getNumbers(firstNum, secondNum);
+    swapValues(firstNum, secondNum);
+    showResults(firstNum, secondNum);
+    return 0;   
+}
+
+void getNumbers(int& input1, int& input2)
+{
+    std::cout << "Enter two integers: ";
+    std::cin >> input1 >> input2;
+}
+void swapValues(int& variable1, int& variable2)
+{
+    int temp{variable1};
+    variable1 = variable2;
+    variable2 = temp;
+}
+void showResults(int output1, int output2)
+{
+    std::cout << "In reverse order the numbers are: " << output1 << " " << output2 << std::endl;
+}
